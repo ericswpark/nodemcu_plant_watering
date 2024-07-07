913b12137a3f11692051dc4eb7291f261006a55b
Eric Park <me@ericswpark.com>
2024-07-07 19:41:20 +0900

    Add OTA over WiFi

--- nodemcu_plant_watering.ino
+++ nodemcu_plant_watering.ino
@@ -4,6 +4,9 @@ Prerequisites:
  - Adafruit ADS1X15 library
 */
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <ESP8266WebServer.h>
 #include <Adafruit_ADS1X15.h>
 
@@ -108,14 +111,36 @@ void setup(void)
   WiFi.setPhyMode(WIFI_PHY_MODE_11G);
   WiFi.begin(ssid, password);
   Serial.print("Connecting to Wi-Fi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Could not connect to WiFi. Rebooting...");
+    delay(3000);
+    ESP.restart();
   }
   Serial.println();
 
   Serial.print("Successfully connected with IP address: ");
   Serial.println(WiFi.localIP());
+
+  // Setup OTA
+  ArduinoOTA.setPassword(ota_pw);
+    ArduinoOTA.onStart([]() {
+    Serial.println("Receiving new firmware through OTA...");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nFinished OTA process.");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 
   server.on("/", handleMain);
   server.on("/s1", handleS1);
